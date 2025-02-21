45c78c8faec1a0e8dcc6afbb18e621e94cc38b47
Ashif Pathan <ashifbgmiking@gmail.com>
2025-02-21 23:08:50 +0530

    Update FinishRide.jsx

--- FRONTEND/src/components/FinishRide.jsx
+++ FRONTEND/src/components/FinishRide.jsx
@@ -9,7 +9,7 @@ const navigate = useNavigate()
 
     async function endRide(){
 
-        const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/rides/end-ride`, {
+        const response = await axios.post(`https://uber-backend-4zhe.onrender.com/rides/end-ride`, {
             rideId: props.ride._id
         }, {
             headers: {
