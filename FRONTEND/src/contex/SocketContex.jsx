b6d9fe616792e52de2a1c5f9ec47f43f4c866bd8
Ashif Pathan <ashifbgmiking@gmail.com>
2025-02-21 23:20:35 +0530

    Update SocketContex.jsx

--- FRONTEND/src/contex/SocketContex.jsx
+++ FRONTEND/src/contex/SocketContex.jsx
@@ -2,7 +2,7 @@ import React, { createContext, useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 
 export const SocketContext = createContext();
-const socket = io(`${import.meta.env.VITE_BASE_URL}`); // change URL as required
+const socket = io(`https://uber-backend-4zhe.onrender.com`); // change URL as required
 const SocketContex = ({ children }) => {
  
 
